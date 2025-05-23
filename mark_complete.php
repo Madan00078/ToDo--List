8e845b34cb07c6fa6c066bf65d44629d24103f2f
Madan00078 <mdnbashyal1234@gmail.com>
2025-05-23 07:57:04 +0545

    Update mark_complete.php

--- mark_complete.php
+++ mark_complete.php
@@ -7,9 +7,7 @@ require 'db.php';
 
 if (isset($_GET['id']) && isset($_GET['status'])) {
     $task_id = $_GET['id'];
-    $status = $_GET['status']; // 0 or 1
-
-    // Update the task status
+    $status = $_GET['status'];
     $stmt = $conn->prepare("UPDATE tasks SET is_completed = :status WHERE id = :id");
     $stmt->bindParam(':status', $status);
     $stmt->bindParam(':id', $task_id);
